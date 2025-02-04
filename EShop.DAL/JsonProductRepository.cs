c931163aad3da3b0496d4602a80faf483e0d3fc1
leschinskiy.av <leschinskiy.av@dns-shop.ru>
2025-02-04 11:53:25 +1000

    сделали совместную работу 3

---
+++ EShop.DAL/JsonProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using EShop.Domain;
+
+namespace EShop.DAL
+{
+    public class JsonProductRepository: IProductRepository
+    {
+        public IEnumerable<Product> Get()
+        {
+            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            var assemblyDirectory = Path.GetDirectoryName(assemblyLocation)!;
+            var JsonFilePath = Path.Combine(assemblyDirectory, "Products.json");
+            var json = File.ReadAllText(JsonFilePath);
+            var products = JsonSerializer.Deserialize<Product[]>(json);
+
+            return products ?? Array.Empty<Product>();
+        }
+    }
+}
